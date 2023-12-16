39520a6e706914474d6567de5a40a4f9e83a5614
Saheed Adeleye <aadesaed@gmail.com>
2023-12-16 18:50:23 +0100

    Added main.ml

--- 23/day2/ocaml/game1/bin/main.ml
+++ 23/day2/ocaml/game1/bin/main.ml
@@ -1,1 +1,22 @@
+open Game1
+open Printf
 
+let main _ =
+  let sum_id acc line =
+    (* get the game *)
+    let g = get_game line in
+    (* sum if possible *)
+    let sum = if game_is_possible g then get_game_id g else 0 in
+    sum + acc
+  in
+  let rec fold_lines f acc ch =
+    match In_channel.input_line ch with
+    | Some line -> fold_lines f (f acc line) ch
+    | None -> acc
+  in
+  let file = open_in Sys.argv.(1) in
+  let res = fold_lines sum_id 0 file in
+  printf "Sum: %d\n" res
+;;
+
+main ()
