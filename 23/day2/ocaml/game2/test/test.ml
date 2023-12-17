6e87ff465a71af4f58f04d293707dfccaadaeb19
Saheed Adeleye <aadesaed@gmail.com>
2023-12-17 07:37:28 +0100

    Setup for part 2: added tests and whatnot (adapted from part 1)

---
+++ 23/day2/ocaml/game2/test/test.ml
@@ -0,0 +1,121 @@
+open OUnit2
+open Game2
+open List
+open Printf
+
+(* TODO: Write better tests than this: inline, expect, property etc *)
+
+(* a pretty printer for tuples because I can't look for one *)
+let ppx_tuple tup =
+  let x, y, z = tup in
+  sprintf "(%d, %d, %d)" x y z
+;;
+
+(* a pretty printer for lists because I can't look for one *)
+let ppx_list lst =
+  let len = length lst in
+  let rec out buf i l =
+    match l with
+    | [] -> buf ^ " ]"
+    | e :: t ->
+      let sl = ppx_tuple e in
+      if i < len - 1 then out (buf ^ sl ^ ", ") (i + 1) t else out (buf ^ sl) (i + 1) t
+  in
+  out "[ " 0 lst
+;;
+
+(* a pretty printer for game type *)
+let ppx_game g = sprintf "{ id = %d; subsets = %s }\n" g.id (ppx_list g.subsets)
+
+let tests =
+  "test suite for game1"
+  >::: [ ("not_fooled_by_flimsy_whitespaces"
+          >:: fun _ ->
+          assert_equal
+            1
+            (get_game_id
+               (get_game "      Game 1      : 3      blue   , 4 red   ,         2 green"))
+         )
+       ; ("can_recognize_game_id"
+          >:: fun _ ->
+          assert_equal
+            1
+            (get_game_id (get_game "Game 1: 3 blue, 4 red, 2 green"))
+            ~printer:string_of_int)
+       ; ("can_store_color_by_value"
+          >:: fun _ ->
+          assert_equal (12, 9, 5) (store_by_color ~color:"red" ~value:9 (3, 9, 5)))
+       ; ("can_overlap_subsets"
+          >:: fun _ ->
+          assert_equal (3, 9, 5) (overlap_subsets (3, 9, 3) (1, 8, 5)) ~printer:ppx_tuple
+         )
+       ; ("can_get_game_subset"
+          >:: fun _ ->
+          assert_equal
+            ({ id = 1235; subsets = [ 4, 0, 3; 1, 2, 6; 0, 2, 0 ] } : game)
+            (get_game "Game 1235: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
+            ~printer:ppx_game)
+       ; ("can_determine_minimum_color_configuration"
+          >:: fun _ ->
+          let _ =
+            assert_equal
+              (4, 22, 6)
+              (minimum_colors
+                 (get_game "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 22 green"))
+              ~printer:ppx_tuple
+          in
+          let _ =
+            assert_equal
+              (4, 2, 6)
+              (minimum_colors
+                 (get_game "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"))
+              ~printer:ppx_tuple
+          in
+          let _ =
+            assert_equal
+              (1, 3, 4)
+              (minimum_colors
+                 (get_game
+                    "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"))
+              ~printer:ppx_tuple
+          in
+          let _ =
+            assert_equal
+              (20, 13, 6)
+              (minimum_colors
+                 (get_game
+                    "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, \
+                     1 red"))
+              ~printer:ppx_tuple
+          in
+          let _ =
+            assert_equal
+              (14, 3, 15)
+              (minimum_colors
+                 (get_game
+                    "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, \
+                     14 red"))
+              ~printer:ppx_tuple
+          in
+          let _ =
+            assert_equal
+              (6, 3, 2)
+              (minimum_colors
+                 (get_game "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"))
+              ~printer:ppx_tuple
+          in
+          ())
+       ]
+;;
+
+(*
+   Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green                    =>  minimum colors = (4, 2, 6)
+   Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue          =>  minimum colors = (1, 3, 4)
+   Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red  =>  minimum colors = (20, 13, 6)
+   Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red  =>  minimum colors = (14, 3, 15)
+   Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green                    =>  minimum colors = (6, 3, 2)
+
+   Possible if and only if: Sum(red) <= 12 AND Sum(green) <= 13 AND Sum(blue) <= 14
+*)
+
+let _ = run_test_tt_main tests
